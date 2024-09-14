2a4fae7d7d5174573ca5528b5b71904a4e07faaa
Irving Yael Agramón Leal <A00833135@itesm.mx>
2024-09-14 16:51:45 -0600

    CarController mejorado

--- ProyectoUwus/Assets/Scripts/CarController.cs
+++ ProyectoUwus/Assets/Scripts/CarController.cs
@@ -4,68 +4,24 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    public float power = 3;
-    public float maxspeed = 5;
-    public float turnpower = 2;
-    public float friction = 3;
-    public Vector2 curspeed;
-    Rigidbody2D rigidbody2D;
+    public float speed = 2.0f;
+    public float rotationSpeed = 100.0f;
+    private Rigidbody2D rb;
 
-    // Use this for initialization
     void Start()
     {
-        rigidbody2D = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
-
 
     void FixedUpdate()
     {
-        curspeed = new Vector2(rigidbody2D.velocity.x, rigidbody2D.velocity.y);
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
 
-        if (curspeed.magnitude > maxspeed)
-        {
-            curspeed = curspeed.normalized;
-            curspeed *= maxspeed;
-        }
+        // Mueve el carro hacia adelante y atrás
+        rb.velocity = transform.right * moveVertical * speed;
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            rigidbody2D.AddForce(transform.right * power);
-            rigidbody2D.drag = friction;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            rigidbody2D.AddForce(-(transform.right) * (power / 2));
-            rigidbody2D.drag = friction;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(Vector3.forward * turnpower);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(Vector3.forward * -turnpower);
-        }
-
-        noGas();
-
-    }
-
-    void noGas()
-    {
-        bool gas;
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
-        {
-            gas = true;
-        }
-        else
-        {
-            gas = false;
-        }
-
-        if (!gas)
-        {
-            rigidbody2D.drag = friction * 2;
-        }
+        // Gira el carro
+        rb.angularVelocity = -moveHorizontal * rotationSpeed;
     }
 }
